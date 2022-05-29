ca756e7f4d0d068acdd046fc43dc6e4daf3fd35f
unknown <mohamedgemy181@gmail.com>
2022-05-29 20:40:46 +0200

    mm

---
+++ startupcode/securitylibrary/MainAlgorithms/Columnar.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    public class Columnar : ICryptographicTechnique<string, List<int>>
+    {
+        public List<int> Analyse(string plainText, string cipherText)
+        {
+            //throw new NotImplementedException();
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+            int n = 0, breaker = 0;
+            int pl = plainText.Length;
+            for (int i = 0; i < pl; i++)//bnmshy 3laa el plain
+            {
+                if (cipherText[0] == plainText[i])//bnshof awl 7arf fi cipher hwa any 7arf fy plain
+                {
+                    int j = i + 1;//bygeb m3a kol lafa el7arf ely b3d el7arf dah
+                    while (j < cipherText.Length)//bn5osh fe loop l 7ad ma elcipher t5ls
+                    {
+                        if (cipherText[1] == plainText[j])//lw tany 7arf fy cipher hwa hwa el7arf ely b3d el7arf elawl
+                        {
+                            int k = j + 1;//nafs klaam nshof ely b3do
+                            while (k < cipherText.Length)//l 7ad ma cipher y5ls
+                            {
+                                if (k - j > j - i)// lw elmsafa ben el k w j akbr mn elmsafa ben j w i
+                                {
+                                    break;//m3nah an msh hwa fa atl3
+                                }
+                                else if (cipherText[2] == plainText[k] && k - j == j - i)// da m3nah an nafs elmsafa benhom
+                                {
+                                    n = j - i;//fa saglna el far2 3shan nemshy beh
+                                    breaker = 1;
+                                    break;
+                                }
+                                k++;
+
+                            }
+                        }
+                        j++;
+                        if (breaker == 1)
+                            break;
+                    }
+                }
+                if (breaker == 1)
+                    break;
+            }
+            int columns = n;//weslna l 3add el coloumns
+            List<int> key = new List<int>(columns);
+            int rows = (int)Math.Ceiling(plainText.Length / (float)n);//3add rows
+            char[,] table = new char[rows, columns];
+            int counter0 = 0;
+            for (int r = 0; r < rows; r++)//bnmla elmatrix b plaintext
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    if (counter0 < plainText.Length)//lw lesa plain m5lstsh bn7ot elba2y fe matrix
+                    {
+                        table[r, c] = plainText[counter0];
+                        counter0++;
+                    }
+                    else//lw 5wlst bnmla b x
+                    {
+                        table[r, c] = 'x';
+                    }
+                }
+            }
+            for (int i = 0; i < columns; i++)//bnmshy 3la coloumns
+            {
+                int checker = 0;
+                int pointer = 0;
+                int counter = 2;
+                for (int j = 0; j < rows; j++)
+                {
+                    //lw el cipher text weslna l a5erha aw el 7arf ely fy matrix hwa hw el7arf ely fy cipher
+                    if ((pointer >= cipherText.Length || table[j, i] == cipherText[pointer]))
+                    {
+                        checker++;//bnzwd el check 3shan n3rf kam 7arf sa7
+                        if (checker >= rows)//3shan n3rf homa sa7 wlaa
+                        { key.Add((int)Math.Ceiling(pointer / (float)rows)); break; }//bnzwd elcolumn dah
+                        pointer++;
+
+                    }
+                    else
+                    {
+                        j = -1;//3shan yerg3 ybd2 mn elawl 3la rows
+                        int counterinc = counter++;
+                        pointer = counterinc * rows - rows;//brg3 elpointer mn tany ll hagm ely kan ablo abl ma y5osh sa7
+                    }
+                }
+            }
+            return key;
+
+        }
+
+        public string Decrypt(string cipherText, List<int> key)
+        {
+            // throw new NotImplementedException();
+            int columns = key.Count;
+            int rows = cipherText.Length / columns;
+            char[,] table = new char[rows, columns];
+            string plain = "";
+            int counter = 1, count2 = 0;
+            for (int c = 0; c < columns; c++)
+            {
+                if (counter == key[c] && counter <= key.Count)
+                {
+                    for (int r = 0; r < rows; r++)
+                    {
+                        if (count2 <= cipherText.Length)
+                        {
+                            table[r, c] = cipherText[count2];
+                            count2++;
+                        }
+                    }
+                    counter++;
+                    c = -1;
+                }
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int z = 0; z < columns; z++)
+                {
+                    plain += table[i, z];
+                }
+            }
+            return plain.ToUpper();
+        }
+
+        public string Encrypt(string plainText, List<int> key)
+        {
+            int columns = key.Count;//b3rf 3add el col mn 3add elkeys
+            int rows = (int)Math.Ceiling((double)plainText.Length / columns);//3add el rows mn ta2semt el kelma 3la elcol
+            char[,] table = new char[rows, columns];//matrix mn row x col
+            string ciphe = "";
+            int mycount = key.Count;
+            int counter = 0;
+            for (int r = 0; r < rows; r++)//blf 3la elmatrix
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    if (counter < plainText.Length)//lw la2et en el counter a2l mn tol elkelma(m3nah elkelma lesa m5lstsh)
+                    {
+                        table[r, c] = plainText[counter];//b7ot fy elmatrix 7arf mn kelmaa
+                        counter++;
+                    }
+                    else//lw elkelma 5elst bmlah elba2y b x
+                    {
+                        table[r, c] = 'x';
+                    }
+                }
+            }
+            Dictionary<int, int> mydic = new Dictionary<int, int>();
+            for (int i = 0; i < mycount; i++)//bmshy 3laa elkey
+            {
+                mydic[key[i] - 1] = i;//b5ly el key zero based
+            }
+            int m = 0;
+            while (m < mycount)//bmshy 3la coloumn
+            {
+                for (int z = 0; z < rows; z++)//bmshy 3la elrows
+                {
+                    ciphe += table[z, mydic[m]];//bzod row w ana msbt elcoloumn 3shan a2ra elcol ely fy key
+                }
+                m++;
+            }
+            return ciphe.ToUpper();
+        }
+    }
+}
---
+++ startupcode/securitylibrary/MainAlgorithms/Monoalphabetic.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    public class Monoalphabetic : ICryptographicTechnique<string, string>
+    {
+        public string Analyse(string plainText, string cipherText)
+        {
+            //throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            char[] key = new char[26];
+            bool[] check_key = new bool[26];
+            int[] aray_of_ascii = new int[1000];
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (plainText[i] == alphabet[j])//lw el 7arf fy plaintext hwa hwa el 7arf fy alphabet
+                    {
+                        key[j] = cipherText[i];//7ot ll 7arf dah elkey b ek7arf ely fy cipher 
+                        check_key[j] = true;//w 2ol en entaa maleto
+                        aray_of_ascii[(int)cipherText[i]] = 1;//3lm ano atmlaa fe ascii
+                    }
+                }
+            }
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (check_key[i] == false)//lw fe haga lesa mgbnash elkey bta3haa mn alphabet
+                {
+                    for (int j = 0; j < alphabet.Length; j++)
+                    {
+                        if (aray_of_ascii[97 + j] != 1)//eh el 7arf ely lesa mt5dsh fe key w ynf3 na5do
+                        {
+                            key[i] = (char)(97 + j);//7oto fe elkey mn alphabet(mashy 3lehom b tarteeb) 
+                            check_key[i] = true;//3lm ano b true fe el alpha en galo key
+                            aray_of_ascii[97 + j] = 1;//3lm ano true k ascii
+                            break;
+                        }
+                    }
+                }
+            }
+            string ret = "";
+            for (int r = 0; r < key.Length; r++)
+            {
+                ret += (char)key[r];// 5azno fe string w ab3to ano el key
+            }
+            // Console.WriteLine(key);
+            return ret;
+        }
+        public string Decrypt(string cipherText, string key)
+        {
+            string ciphe = cipherText.ToLower();
+            //throw new NotImplementedException();
+            char[] plaint = new char[cipherText.Length];
+            char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                for (int j = 0; j < key.Length; j++)
+                {
+                    if (ciphe[i] == key[j])
+                    {
+                        plaint[i] = alphabet[j];
+                    }
+                }
+            }
+            return new string(plaint);
+        }
+
+        public string Encrypt(string plainText, string key)
+        {
+            char[] citext = new char[plainText.Length];
+            char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                for (int j = 0; j < key.Length; j++)
+                {
+                    if (plainText[i] == alphabet[j])//bmshy 3laa plain text w ashof el7arf dah any 7arf fy el alpha w ashel w a7ot el7arf
+                                                    //ely b nafs elindex bta3o fy elkey
+                    {
+                        citext[i] = key[j];
+                    }
+                }
+            }
+            return new string(citext);
+        }
+
+        /// <summary>
+        /// Frequency Information:
+        /// E   12.51%
+        /// T	9.25
+        /// A	8.04
+        /// O	7.60
+        /// I	7.26
+        /// N	7.09
+        /// S	6.54
+        /// R	6.12
+        /// H	5.49
+        /// L	4.14
+        /// D	3.99
+        /// C	3.06
+        /// U	2.71
+        /// M	2.53
+        /// F	2.30
+        /// P	2.00
+        /// G	1.96
+        /// W	1.92
+        /// Y	1.73
+        /// B	1.54
+        /// V	0.99
+        /// K	0.67
+        /// X	0.19
+        /// J	0.16
+        /// Q	0.11
+        /// Z	0.09
+        /// </summary>
+        /// <param name="cipher"></param>
+        /// <returns>Plain text</returns>
+        struct maker
+        {
+            public int counter;
+            public char myalpha;
+            public char secalpha;
+        }
+        maker[] yarab = new maker[26];
+        public string AnalyseUsingCharFrequency(string cipher)
+        {
+            //throw new NotImplementedException();
+            string freq = "etaoinsrhldcumfpgwybvkxjqz";
+            char[] plaine = new char[cipher.Length];
+            string ciphe = cipher.ToLower();
+            for (int i = 0; i < 26; i++)//bmshy mn awl el a fy ascii w a7ot kol el 7rof fy el array
+            {
+                int ind = 97 + i;
+                yarab[i].myalpha = (char)ind;
+            }
+            for (int j = 0; j < cipher.Length; j++)//bmshy 3la cipher
+            {
+                for (int l = 0; l < 26; l++)//bmshy 3la el7roof
+                {
+                    if (yarab[l].myalpha == ciphe[j])///bzwd el counter ll 7arf kol ma ytkrr fy text
+                    {
+                        yarab[l].counter++;
+                    }
+                }
+            }
+            for (int z = 0; z < 26; z++)
+            {
+                for (int k = z + 1; k < 26; k++)
+                {
+                    if (yarab[z].counter < yarab[k].counter)//b3dy 3la el7arf w ely b3do w b sort b el counter
+                    {
+                        char temp = yarab[z].myalpha;
+                        int temp2 = yarab[z].counter;
+                        yarab[z].myalpha = yarab[k].myalpha;
+                        yarab[z].counter = yarab[k].counter;
+                        yarab[k].myalpha = temp;
+                        yarab[k].counter = temp2;
+                    }
+                }
+            }
+            for (int m = 0; m < 26; m++)//b7ot 7rof freq
+            {
+                yarab[m].secalpha = freq[m];
+            }
+            for (int n = 0; n < cipher.Length; n++)
+            {
+                for (int v = 0; v < 26; v++)
+                {
+                    if (ciphe[n] == yarab[v].myalpha)//bshof kol 7arf fy cipher zay el7arf fy alpha
+                    {
+                        plaine[n] = yarab[v].secalpha;//a7ot fy plaine el7arf ely zayo mn freq
+                    }
+                }
+            }
+            return new string(plaine);
+        }
+    }
+}
